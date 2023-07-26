6cd5c06140ac325c7916a4a7752ef90531f93c39
DHARSHA S R <100016444+Dharsha04@users.noreply.github.com>
2023-07-26 18:24:04 +0530

    Update ReverseDigit.java

--- ReverseDigit.java
+++ ReverseDigit.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int reverse(int x) {
+        int rev=0;
+        int temp=x;
+        while(x!=0){
+            int rem=x%10;
+            x=x/10;
+            if(rev>Integer.MAX_VALUE/10)
+                 return 0;
+            if(rev<Integer.MIN_VALUE/10)
+                 return 0;
+            rev=rev*10+rem;
+        }
+        return rev;
+    }
+}
